cc729c8d958705f02e1f4a15670c3b3ee10fa181
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-04-18 22:16:22 +0700

    Feature Check Activity passed at splash screen

--- lib/src/ui/screens/splash_screen.dart
+++ lib/src/ui/screens/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './welcome_screen.dart';
 
@@ -7,8 +8,24 @@ import '../template/splashscreen_template/splashscreen_template.dart';
 import '../template/splashscreen_template/widgets/splashscreen_image_asset.dart';
 import '../variable/config/app_config.dart';
 
-class SplashScreen extends StatelessWidget {
+import '../../providers/main_calendar_provider.dart';
+
+class SplashScreen extends StatefulWidget {
   static const routeNamed = '/splashscreen';
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    final mainCalendarProvider =
+        Provider.of<MainCalendarProvider>(context, listen: false);
+    mainCalendarProvider.updateStatusWhenDatePassed();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
