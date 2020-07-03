3643c8f113fff3d5d9076b41ad6fff8cf64d0ddc
Zeffry Reynando <zeffry.reynando@gmail.com>
2020-07-03 20:14:07 +0700

    Firebase Remote Config

--- lib/src/function/update_app_playstore.dart
+++ lib/src/function/update_app_playstore.dart
@@ -5,9 +5,12 @@ class UpdateAppPlaystore {
 
   Future<bool> isNeedUpdate(String currentBuildVersion) async {
     RemoteConfig remoteConfig = await RemoteConfig.instance;
-    await remoteConfig.fetch();
+    //! Harus di setting agar value dari firebase remote config tidak di cache, setting menjadi 0 second.
+    await remoteConfig.fetch(expiration: Duration(seconds: 0));
     await remoteConfig.activateFetched();
     final newestBuildVersion = remoteConfig.getString(firebaseRemoteKey);
+    print(
+        "Current Version $currentBuildVersion || NewestVersion $newestBuildVersion");
     if (newestBuildVersion != currentBuildVersion) {
       print('Butuh Update');
       return true;
